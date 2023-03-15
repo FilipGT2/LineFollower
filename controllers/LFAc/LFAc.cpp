ece7a7a63abfb22e6a0b404b9c606a3c0dfc1a4c
Filip Najdovsi <81031992+FilipGT2@users.noreply.github.com>
2023-03-15 22:12:54 +0100

    Izmeni

--- controllers/LFAc/LFAc.cpp
+++ controllers/LFAc/LFAc.cpp
@@ -42,11 +42,11 @@ Motor *right_motor = robot->getMotor("right wheel motor");
 left_motor->setPosition(INFINITY);
 right_motor->setPosition(INFINITY);
 
-left_motor->setVelocity(0,0);
-right_motor->setVelocity(0,0);
+left_motor->setVelocity(0.0);
+right_motor->setVelocity(0.0);
 
-DistanceSensor *left_ir = robot->getDistanceSensor("ir0");
-DistanceSensor *right_ir = robot->getDistanceSensor("ir1");
+DistanceSensor *left_ir = robot->getDistanceSensor("Ir1");
+DistanceSensor *right_ir = robot->getDistanceSensor("Ir2");
 
 left_ir->enable(TIME_STEP);
 right_ir->enable(TIME_STEP);
@@ -56,15 +56,32 @@ double right_speed = MAX_SPEED;
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
-  while (robot->step(timeStep) != -1) {
+  while (robot->step(TIME_STEP) != -1) {
     // Read the sensors:
     // Enter here functions to read sensor data, like:
-    //  double val = ds->getValue();
-
+    //  double val = ds->getValue();_
+    double left_ir_val = left_ir->getValue();
+    double right_ir_val = right_ir->getValue();
+    
+    if((left_ir_val > right_ir_val) && line_left)
+    {
+      left_speed = MAX_SPEED * 0.25;
+    }
+    else if ((right_ir_val -> left_ir_val) && line_right)
+    {
+    right_speed = MAX_SPEED * 0.25;
+    }
+ 
     // Process sensor data here.
-
+    left_speed = MAX_SPEED;
+    right_speed = MAX_SPEED;
+    
+    bool line_left = (4<left_ir_val) && (left_ir_val <15);
+    bool line_right = (4<right_ir_val) && (right_ir_val <15);
     // Enter here functions to send actuator commands, like:
     //  motor->setPosition(10.0);
+    left_motor->setVelocity(left_speed);
+    right_motor->setVelocity(right_speed);
   };
 
   // Enter here exit cleanup code.
